650429ae29f24052d99ad518b5eb206140fea1fe
Farouk Khaled <80914530+FarouqKhaled17@users.noreply.github.com>
2022-12-18 22:52:02 +0200

    final 1

--- pc/pc/src/pc/BoundedBuffer.java
+++ pc/pc/src/pc/BoundedBuffer.java
@@ -2,39 +2,36 @@
 package pc;
 import java.lang.System.Logger;
 import java.lang.System.Logger.Level;
-import java.util.Arrays;
 import java.util.Date;
 import java.util.concurrent.Semaphore;
  
 class BoundedBuffer implements Buffer{ 
     int item;
-      private static final int BUFFER_SIZE = 3; //max size of buffer array
-      private int count; //number of items currently in the buffer
-      private int in;   // points to the next free position in the buffer
-      private int out;  // points to the first filled position in the buffer
-      private Object[] buffer; //array of Objects
-      private Semaphore mutex; //provides limited access to the buffer (mutual exclusion)
-      private Semaphore empty; //keep track of the number of empty elements in the array
-      private Semaphore full; //keep track of the number of filled elements in the array
+      private static final int BUFFER_SIZE = 3;
+      private int count;
+      private int in;
+      private int out;
+      private Object[] buffer;
+      private Semaphore mutex;
+      private Semaphore empty;
+      private Semaphore full;
        
        public BoundedBuffer(){
-      // buffer is initially empty
          item=0;
          count = 0;
          in = 0;
          out = 0;
          buffer = new Object[BUFFER_SIZE];
-         mutex = new Semaphore(1); //1 for mutual exclusion
-         empty = new Semaphore(BUFFER_SIZE); //array begins with all empty elements
-         full = new Semaphore(0); //array begins with no elements
+         mutex = new Semaphore(1);
+         empty = new Semaphore(BUFFER_SIZE);
+         full = new Semaphore(0);
       }
-   
-   // producer calls this method
+
        public void insert() {
 
          try{
-            empty.acquire(); //keep track of number of empty elements (value--)
-            mutex.acquire(); //mutual exclusion
+            mutex.acquire();
+            empty.acquire();
          }
              catch (InterruptedException e) { 
                System.out.println("ERROR in insert(): " + e);
@@ -43,7 +40,6 @@ class BoundedBuffer implements Buffer{
            System.out.println("Thread: "+Thread.currentThread().getName()  +" in critical section "+date);
 
 
-           // add an item to the buffer
          ++count;
          buffer[in] = item;
          in = (in + 1) % BUFFER_SIZE;
@@ -51,13 +47,12 @@ class BoundedBuffer implements Buffer{
            item++;
       
        
-         mutex.release(); //mutual exclusion
+         mutex.release();
            System.out.println("Thread: "+Thread.currentThread().getName()  +" left critical section");
 
-           full.release(); //keep track of number of elements (value++)
-           System.out.println( Thread.currentThread().getName() +"turn"+ Arrays.toString(buffer)+"-> "+count);
+           full.release();
 
-           //If buffer was empty, then this wakes up the Consumer
+
           try {
               Thread.sleep(2000);
           } catch (InterruptedException ex) {
@@ -65,13 +60,13 @@ class BoundedBuffer implements Buffer{
           }
        }
    
-   // consumer calls this method
+
        public Object remove() {
          Object item=null;
 
        try{
-            mutex.acquire(); //mutual exclusion
-            full.acquire(); //keep track of number of elements (value--)
+            mutex.acquire();
+            full.acquire();
 
 
          }
@@ -81,21 +76,20 @@ class BoundedBuffer implements Buffer{
            Date date= new Date();
            System.out.println("Thread: "+Thread.currentThread().getName()  +" in the critical section "+date);
       	
-      // remove an item from the buffer
+
          --count;
          item = buffer[out]; 
          out = (out + 1) % BUFFER_SIZE;
 
     System.out.println("Consumer consumed  " + item );
 
-         mutex.release(); //mutual exclusion
+         mutex.release();
            System.out.println("Thread: "+Thread.currentThread().getName()  +" left the critical section");
 
-           empty.release(); //keep track of number of empty elements (value++)
-           System.out.println( Thread.currentThread().getName()  +"turn "+ item+"-> "+count);
+           empty.release();
 
 
-           //if buffer was full, then this wakes up the Producer
+
           try {
               Thread.sleep(3000);
           } catch (InterruptedException ex) {
--- pc/pc/src/pc/Buffer.java
+++ pc/pc/src/pc/Buffer.java
@@ -2,17 +2,7 @@
 package pc;
 
 interface Buffer{
-   /**
-    * insert an item into the Buffer.
-    * Note this may be either a blocking
-    * or non-blocking operation.
-    */
        public abstract void insert();
-   
-   /**
-    * remove an item from the Buffer.
-    * Note this may be either a blocking
-    * or non-blocking operation.
-    */
+
        public abstract Object remove();
    }
